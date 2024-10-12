791ecf72c3ec2d0884764a644f4eac87a3fe3fbe
kaanimral <149240588+kaanimral@users.noreply.github.com>
2024-10-12 20:02:46 +0300

    Add files via upload

---
+++ javaBtkAkademi/src/javabtkakademi/javaBtkAkademi.java
@@ -0,0 +1,13 @@
+package javabtkakademi;
+
+
+public class javaBtkAkademi {
+    
+    
+    public static void main(String[] args){
+        
+        System.out.println("Hello Java"); 
+        
+    }
+}
+
